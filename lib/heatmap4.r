ed8fca0a8329fd5c8edcde9486f1190ce316cae6
guillermodeandajauregui@gmail.com <guillermodeandajauregui@gmail.com>
2016-09-17 12:31:39 -0500

    new heatmap function, heatmap4.r

--- lib/heatmap4.r
+++ lib/heatmap4.r
@@ -1,53 +1,37 @@
 # Primero hay que cargar los archivos csv a una tabla de R con su encabezado y todo
 
 #args <- commandArgs(trailingOnly = TRUE);
-args<-"pruebas_enrichment_module/prueba_I001/triphop_GOBP.csv"
+#args<-"pruebas_enrichment_module/intento/gansoz_GOMF.csv"
+#args<-"pruebas_enrichment_module/intento/emptyframe.csv"
+args<- "pruebas_enrichment_module/prueba_I001/triphop_GOBP.csv"
 DATA = args[1];
 
 # X=read.table("Archivo.csv", header=TRUE, sep =",")
-X=read.table(DATA, header=TRUE, sep ="\t");
+X=read.table(DATA, header=TRUE, sep ="\t", row.names = 1);
+Y = as.matrix(X)
+Z = -log10(Y) #mo color, mo statistical significance
 
-# Luego hay que definir la columna de nombres 
-#(columna 1 de su csv cargado en el objeto X) 
-#en un objeto que llamaré RN (de row names)
-
-RN=X[,1];
-END = length(X[0,]) - 2;
-
-# Los datos numéricos de su matríz están en las columnas 2 a la 31, 
-#carguenlos en un objeto Y
-
-# Y = X[,2:31]
-Y=X[,2:END];
-
-Y = as.matrix(Y);
-Y = as.numeric(Y);
-
-# Pero en su excel pusieron p-values y nosotros queremos que a 
-#mayor significancia más intensidad de color, 
-#eso se logra con el -log10(p-value)
-
-Z=-log10(Y);
-
-# Para hacer el heatmap deberán tener cargada la libraría (gplots) en [R]
+if(ncol(Z)==1){
+  Z = cbind(Z,Z)
+  colnames(Z)[2] <- paste("FakeColforceheatmap")
+  #return(Z)
+}
 
 library(gplots);
 
-# Es buena idea definir una paleta de colores, n es el número de intervalos de color
-
-
+# color palette
 x = strsplit(DATA, "_")
 y <- as.character(x[[1]])
 z = strsplit(y[length(y)], ".csv")
 
 
-if (z[1] == 'CC') {
+if (z[1] == 'GOCC') {
   my_palette <- colorRampPalette(c("white", "violet", "darkviolet", "midnightblue"))(n = 1000);
-} else if (z[1] == 'BP') {
+} else if (z[1] == 'GOBP') {
   my_palette <- colorRampPalette(c("white", "greenyellow", "limegreen", "darkgreen"))(n = 1000);
-} else if (z[1] == 'MF') {
+} else if (z[1] == 'GOMF') {
   my_palette <- colorRampPalette(c("white", "lightsalmon", "orangered", "darkred"))(n = 1000); 
-} else if (z[1] == 'kegg') {
+} else if (z[1] == 'KEGG') {
   my_palette <- colorRampPalette(c("white", "lightskyblue", "steelblue", "darkblue"))(n = 1000);
 } else {
   my_palette <- colorRampPalette(c("white", "lightcoral", "firebrick", "red"))(n = 1000);
@@ -66,7 +50,14 @@ pdf(out, paper="USr");
 
 
 # heatmap.2(Z, trace="none", col=my_palette, scale="row", labCol=colnames(Z), labRow=RN);
-heatmap.2(Z, trace="none", col=my_palette, labRow=RN, labCol=colnames(Z), margins = c(5, 11.5), cexCol = 0.6, cexRow = 0.3);
+heatmap.2(Z, 
+          trace="none", 
+          col=my_palette, 
+          labRow=rownames(Z), 
+          labCol=colnames(Z), 
+          margins = c(5, 11.5), 
+          cexCol = 0.4, 
+          cexRow = 0.3);
 
 dev.off();
 
